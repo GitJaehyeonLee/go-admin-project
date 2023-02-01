bd2147432be95b5c65df560eb62665b91768e0d5
jaylee <kerneltrek@gmail.com>
2023-02-01 23:31:44 +0900

    Add Register & Password crypt

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,16 +2,34 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"go-admin/database"
 	"go-admin/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *fiber.Ctx) error {
-	user := models.User{
-		FirstName: "John",
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "passwords do not match",
+		})
 	}
 
-	user.LastName = "Doe"
-	user.Email = "example.com"
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	user := models.User{
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  password,
+	}
+
+	database.DB.Create(&user)
 
 	return c.JSON(user)
 }
--- database/connect.go
+++ database/connect.go
@@ -6,6 +6,8 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 // Public Function 은 앞에 대문자로 기록 / 소문자는 프라이빗
 func Connect() {
 	database, err := gorm.Open(mysql.Open("root:test1234@/go_admin"), &gorm.Config{})
@@ -13,6 +15,9 @@ func Connect() {
 	if err != nil {
 		panic("Could not connet to the database")
 	}
+
+	DB = database
+
 	database.AutoMigrate(&models.User{})
 
 }
