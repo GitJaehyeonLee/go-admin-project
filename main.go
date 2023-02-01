ef73fafc74edbc05ca5e4912ee339b81582ed567
jaylee <kerneltrek@gmail.com>
2023-02-01 23:49:29 +0900

    JWT 토큰 쿠키 기록

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go-admin/database"
 	"go-admin/routes"
 )
@@ -9,6 +10,10 @@ import (
 func main() {
 	database.Connect()
 	app := fiber.New()
+	// cookie 데이터 접근 허용
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
