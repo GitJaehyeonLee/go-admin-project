9186e76983e996a0eac7684cb3da2cb467f64c2e
jaylee <kerneltrek@gmail.com>
2023-02-02 14:01:19 +0900

    [Day 2] Image Upload 기능 추가

--- controllers/productController.go
+++ controllers/productController.go
@@ -4,30 +4,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go-admin/database"
 	"go-admin/models"
-	"math"
 	"strconv"
 )
 
 func AllProduct(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
-	limit := 5
-	offset := (page - 1) * limit
-	var total int64
-
-	var products []models.Product
-
-	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&products)
-
-	database.DB.Model(&models.User{}).Count(&total)
-
-	return c.JSON(fiber.Map{
-		"data": products,
-		"meta": fiber.Map{
-			"total":     total,
-			"page":      page,
-			"last_page": math.Ceil(float64(int(total) / limit)),
-		},
-	})
+	return c.JSON(models.Paginate(database.DB, &models.Product{}, page))
 }
 
 // 관리자가 사용자 만드는 기능
